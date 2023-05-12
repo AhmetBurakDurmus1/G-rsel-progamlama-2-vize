7791864c07a2c9e45e0d9009fe6e3a387d8ca58c
ahmet burak <133243418+AhmetBurakDurmus1@users.noreply.github.com>
2023-05-13 00:03:29 +0300

    vize ödevi
    vize ödevi degisiklik.

---
+++ Ahmet Burak Durmus-Görsel Programlama 2-Vize/vizeödeviUcakrezervasyon/Rezervasyon.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace vizeödeviUcakrezervasyon
+{
+    partial class Rezervasyon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Rezervasyon
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Rezervasyon";
+            this.Load += new System.EventHandler(this.Rezervasyon_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem anaMenüToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbSefer;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dataGridViewSeferler;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox koltuklar;
+    }
+}
